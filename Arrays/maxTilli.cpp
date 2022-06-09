861f4c495226a2daeda08d82bd39efc0ebdcc9dc
Mayank Raj <63256892+rockermayanks@users.noreply.github.com>
2022-06-09 15:43:30 +0530

    Add files via upload

---
+++ Arrays/maxTilli.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int mx=-1999999999;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for(int i=0;i<n;i++){
+        mx=max(mx,arr[i]);
+        cout<<mx<<" ";
+    }
+
+}
